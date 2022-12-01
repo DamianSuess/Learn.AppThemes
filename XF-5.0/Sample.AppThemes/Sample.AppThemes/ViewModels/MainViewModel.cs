c070e65ca4bfde94045b5996a36ed62e19eba796
Damian Suess <djsuess@gmail.com>
2022-12-01 12:37:03 -0500

    Report current theme

--- XF-5.0/Sample.AppThemes/Sample.AppThemes/ViewModels/MainViewModel.cs
+++ XF-5.0/Sample.AppThemes/Sample.AppThemes/ViewModels/MainViewModel.cs
@@ -1,19 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Prism.Commands;
-using Prism.Mvvm;
-using Prism.Navigation;
+﻿using Prism.Navigation;
+using Xamarin.Essentials;
 
 namespace Sample.AppThemes.ViewModels
 {
   public class MainViewModel : ViewModelBase
   {
     public MainViewModel(INavigationService navigationService)
-        : base(navigationService)
+      : base(navigationService)
     {
       Title = "Main Page";
     }
+
+    public string AppName => AppInfo.Name;
+
+    public string AppPackageName => AppInfo.PackageName;
+
+    public string AppTheme => AppInfo.RequestedTheme.ToString();
   }
 }
